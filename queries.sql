3ebe6d1e0091905af5844eced2fa8ee3bf907c12
karolinagorod-commits <karolinagorod@gmail.com>
2026-03-20 20:36:33 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,28 +1,19 @@
 SELECT COUNT(customer_id) AS customers_count
 FROM customers;
 
-WITH tab AS (
-    SELECT
-        s.sales_person_id,
-        -- объединяем имя и фамилию, без лишних пробелов
-        TRIM(e.first_name) || ' ' || TRIM(e.last_name) AS seller,
-        -- количество сделок
-        COUNT(*) AS operations,
-        -- считаем выручку продавца за всё время
-        FLOOR(SUM(p.price * s.quantity)) AS income
-    FROM sales AS s
-    INNER JOIN products AS p ON s.product_id = p.product_id
-    -- соединяем таблицы
-    INNER JOIN employees AS e ON s.sales_person_id = e.employee_id
-    GROUP BY s.sales_person_id, seller
-)
-
 SELECT
-    t.seller,
-    t.operations,
-    t.income
-FROM tab AS t
-ORDER BY t.income DESC
+    -- объединяем имя и фамилию, без лишних пробелов
+    TRIM(e.first_name) || ' ' || TRIM(e.last_name) AS seller,
+    -- количество сделок
+    COUNT(*) AS operations,
+    -- считаем выручку продавца за всё время
+    FLOOR(SUM(p.price * s.quantity)) AS income
+FROM sales AS s
+INNER JOIN products AS p ON s.product_id = p.product_id
+-- соединяем таблицы
+INNER JOIN employees AS e ON s.sales_person_id = e.employee_id
+GROUP BY seller
+ORDER BY income DESC
 LIMIT 10;
 
 WITH seller_avg AS (
@@ -41,49 +32,39 @@ SELECT
     FLOOR(s.average) AS average_income
 FROM seller_avg AS s
 -- выборка продавцов с меньшей средней выручкой
-WHERE s.average < (SELECT AVG(sa.average) FROM seller_avg AS sa)
+WHERE s.average < (
+    -- среднее от исходных данных
+    SELECT AVG(p.price * sa.quantity)
+    FROM sales AS sa
+    INNER JOIN products AS p ON sa.product_id = p.product_id
+)
 ORDER BY s.average ASC;
 
-WITH day_sales AS (
-    SELECT
-        TRIM(e.first_name) || ' ' || TRIM(e.last_name) AS seller,
-        -- преобразовываем дату в день недели
-        TRIM(TO_CHAR(s.sale_date, 'day')) AS day_of_week,
-        -- задаём порядковый номер каждому дню
-        EXTRACT(ISODOW FROM s.sale_date) AS day_number,
-        -- считаем выручку по дням
-        FLOOR(SUM(p.price * s.quantity)) AS income
-    FROM sales AS s
-    INNER JOIN products AS p ON s.product_id = p.product_id
-    INNER JOIN employees AS e ON s.sales_person_id = e.employee_id
-    GROUP BY seller, day_of_week, day_number
-)
-
--- итоговый запрос
 SELECT
-    d.seller,
-    d.day_of_week,
-    d.income
-FROM day_sales AS d
-ORDER BY d.day_number ASC, d.seller ASC;
+    TRIM(e.first_name) || ' ' || TRIM(e.last_name) AS seller,
+    -- преобразовываем дату в день недели
+    TRIM(TO_CHAR(s.sale_date, 'day')) AS day_of_week,
+    -- считаем выручку по дням
+    FLOOR(SUM(p.price * s.quantity)) AS income
+FROM sales AS s
+INNER JOIN products AS p ON s.product_id = p.product_id
+INNER JOIN employees AS e ON s.sales_person_id = e.employee_id
+GROUP BY
+    seller,
+    day_of_week,
+    EXTRACT(ISODOW FROM s.sale_date)
+ORDER BY EXTRACT(ISODOW FROM s.sale_date) ASC, seller ASC;
 
-WITH age_groups AS (
-    SELECT
-        customer_id,
-        -- age_category с тремя диапазонами возрастов
-        CASE
-            WHEN age BETWEEN 16 AND 25 THEN '16-25'
-            WHEN age BETWEEN 26 AND 40 THEN '26-40'
-            WHEN age > 40 THEN '40+'
-        END AS age_category
-    FROM customers
-)
-
--- считаем количество человек в каждом диапазоне
 SELECT
-    age_category,
+    -- age_category с тремя диапазонами возрастов
+    CASE
+        WHEN age BETWEEN 16 AND 25 THEN '16-25'
+        WHEN age BETWEEN 26 AND 40 THEN '26-40'
+        WHEN age > 40 THEN '40+'
+    END AS age_category,
+    -- считаем количество человек в каждом диапазоне
     COUNT(*) AS age_count
-FROM age_groups
+FROM customers
 GROUP BY age_category
 ORDER BY age_category ASC;
 
@@ -114,3 +95,6 @@ INNER JOIN customers AS c ON s.customer_id = c.customer_id
 WHERE p.price = 0
 -- сортируем по id и первой дате покупки
 ORDER BY c.customer_id ASC, s.sale_date ASC;
+
+
+
